8c1d1b2a0d46b330f48ff7460bc98bfcfc4b5ce3
Marc Leplattenier <marcol.lepl@gmail.com>
2021-08-02 13:19:49 +0200

    2nd ex done

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,18 @@
+class QuestionsController < ApplicationController
+  def ask
+  end
+
+  def answer
+    @question = params[:question]
+    @answers = []
+    if @question == 'I am going to work'
+      @answers = 'Great!'
+
+    elsif @question.end_with?('?')
+      @answers = 'Silly question, get dressed and go to work!'
+
+    else
+      @answers = "I don't care, get dressed and go to work!"
+    end
+  end
+end
